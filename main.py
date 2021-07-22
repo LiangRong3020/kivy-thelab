37d6fe989f6d1cc7363b560573cc85b30a2871e7
張良榮 <juce810282@gmail.com>
2021-07-23 00:46:56 +0800

    make buttons on screan

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    def build(self):
+        pass
+
+
+
+class TheAppLab(App):
+    pass
+
+
+
+TheAppLab().run()
