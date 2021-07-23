4cee9609ee89b17227a453d5909713ec0d7a5382
張良榮 <juce810282@gmail.com>
2021-07-23 21:33:31 +0800

    Layout and Scroll View

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+class GridLayoutExample(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.cols= 2
+        for i in range(50):
+            b= Button(text= str(i), size_hint_y=None, width=100)
+            self.add_widget(b)
+
+class BoxLayoutExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.orientation= "vertical"
+
+        for i in range(50):
+            b= Button(text= str(i), size_hint= (1, 1), height= "100dp")
+            self.add_widget(b)
 
 class MainWidget(Widget):
     def build(self):
