3bd507dcdbdeb5f936c270e09c5ecb0719f8b606
張良榮 <juce810282@gmail.com>
2021-07-26 00:04:25 +0800

    add switch textInput slider ..

--- main.py
+++ main.py
@@ -4,16 +4,48 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty
 
 
 class WidgetsExample(GridLayout):
 
     count_= 0
     label_text= StringProperty(str(count_))
+    widget_state= BooleanProperty(False)
+
+    # slder
+    slider_state = BooleanProperty(False)
+    slider_text = StringProperty(str(50))
+
+    # text input
+    text_input_string= StringProperty()
+
     def click_button(self):
-        self.count_ += 1
-        self.label_text= str(self.count_)
+        if self.widget_state:
+            self.count_ += 1
+            self.label_text= str(self.count_)
         # print('you pressed')
+
+    def on_toggle_button_state(self, widget):
+
+        if widget.state == 'normal':
+            self.widget_state = False
+            widget.text = 'OFF'
+        else:
+            self.widget_state = True
+            widget.text = 'ON'
+
+    # def on_switch_active(self, widget):
+    #     if widget.active:
+    #         self.slider_state = True
+    #     else:
+    #         self.slider_state = False
+
+    # def on_slider_value(self, widget):
+    #     self.slider_text= str(round(widget.value, 2))
+
+    def on_text_validate(self, widget):
+        self.text_input_string= widget.text
 
 class GridLayoutExample(GridLayout):
     def __init__(self, **kwargs):
