9487b57c4931bc842d923106b3167ff0a0d8a37f
張良榮 <juce810282@gmail.com>
2021-07-27 00:29:07 +0800

    add canvas and move it

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import StringProperty
-from kivy.properties import BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, Clock
+from kivy.graphics.vertex_instructions import Line
+from kivy.graphics import Color, Rectangle, Ellipse
+from kivy.metrics import dp
 
 
 class WidgetsExample(GridLayout):
@@ -70,7 +72,76 @@ class MainWidget(Widget):
     def build(self):
         pass
 
+class canvasExample1(Widget):
+    pass
 
+class canvasExample2(Widget):
+    pass
+
+class canvasExample3(Widget):
+    pass
+
+class canvasExample4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.count_= 0
+        with self.canvas:
+            Line(points= (100, 100, 400, 500), width= 2)
+            Color(0, 1, 0)
+            Line(circle= (400, 200, 80), width= 2)
+
+            Line(rectangle=(700, 200, 150, 100), width=2)
+            self.rect= Rectangle(pos=(700, 500), size=(150, 100))
+
+    def on_press_a_click(self):
+        x, y= self.rect.pos
+        w, h= self.rect.size
+        inc = dp(10)
+        x =  x + inc if x + inc+ w <= self.width else self.width-w
+
+        self.rect.pos= (x, y)
+
+        # self.label_text= str(self.count_)
+
+class canvasExample5(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size= dp(50)
+        self.vx = 3
+        self.vy = 4
+        with self.canvas:
+            self.ball = Ellipse(pos= self.center, size= (self.ball_size, self.ball_size))
+
+        Clock.schedule_interval(self.update, 1/60)
+
+    def on_size(self, *args):
+        # print('width: ', self.width, ' height: ', self.height)
+        self.ball.pos= (self.center_x- self.ball_size/2, self.center_y- self.ball_size/2)
+
+    def update(self, dt):
+
+        x, y= self.ball.pos
+
+        x += self.vx
+        y += self.vy
+
+        if y + self.ball_size >= self.height:
+            y= self.height-self.ball_size
+            self.vy *= -1
+        elif y <= 0:
+            y = 0
+            self.vy *= -1
+
+
+        if x + self.ball_size >= self.width:
+            x = self.width - self.ball_size
+            self.vx *= -1
+        elif x <= 0:
+            x = 0
+            self.vx *= -1
+
+
+        self.ball.pos= (x, y)
 
 class TheAppLab(App):
     pass
